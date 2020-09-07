d7732c82fb62a7bb16d3818292bda23aedf54c0f
LK920 <hopeone1214@gmail.com>
2020-09-07 14:01:38 +0900

    Ch08 mybatis

---
+++ Ch08/src/main/java/kr/co/ch08/dao/UserDAO.java
@@ -0,0 +1,54 @@
+package kr.co.ch08.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import kr.co.ch08.vo.UserVO;
+
+@Repository
+public class UserDAO {
+	
+	@Inject
+	private JdbcTemplate jdbc;
+	@Inject
+	private SqlSessionTemplate mybatis;
+	
+	public void insertUser(UserVO vo) {
+		//jdbc 规侥
+		//String sql = "INSERT INTO `USER3` VALUES (?,?,?,?)";
+		//Object[] values = {vo.getUid(), vo.getName(), vo.getHp(), vo.getAge()};
+		
+		//jdbc.update(sql, values);
+		
+		//mybatis 规侥
+		mybatis.insert("mapper.user.INSERT_USER", vo);
+	}
+	
+	public UserVO selectUser(String uid) {
+		return mybatis.selectOne("mapper.user.SELECT_USER", uid);
+	}
+	
+	public List<UserVO> selectUsers() {
+		
+		//Spring jdbc 规侥
+		//String sql = "SELECT * FROM `USER3`";
+		//List<UserVO> users = jdbc.query(sql, new UserRowMapper()); 
+	
+		//mybatis 规侥
+		List<UserVO> users = mybatis.selectList("mapper.user.SELECT_USERS");
+		return users;
+	}
+	
+	public void updatetUser(UserVO vo) {
+		mybatis.update("mapper.user.UPDATE_USER", vo);
+	}
+	public void deleteUser(String uid) {
+		mybatis.delete("mapper.user.DELETE_USER", uid);
+	}
+
+}
